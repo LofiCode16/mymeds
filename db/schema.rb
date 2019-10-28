a53769c0711a6ef17a6a18886fa46fd2381165b9
diegoski007 <diegopr178@gmail.com>
2019-10-28 15:04:41 -0300

    merge done

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 2019_10_28_152326) do
-=======
 ActiveRecord::Schema.define(version: 2019_10_28_172814) do
->>>>>>> medics
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
