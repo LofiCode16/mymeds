99406c280dafc02d921ab79620aa86e1c4bce46c
diegoski007 <diegopr178@gmail.com>
2019-10-29 12:08:54 -0300

    add delete in done

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,9 +7,9 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-
+require('packs/medictasks.js')
 require("packs/docs.js.erb")
-require('packs/completed_medictask.js')
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
--- app/javascript/packs/completed_medictask.js
+++
@@ -1,5 +0,0 @@
-$(document).on('ready turbolinks: load', function(){
-  $('#done').on('click', function(){
-    $('#date').toggle(500);
-  });
-});
