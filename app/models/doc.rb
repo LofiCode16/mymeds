ac0455d9422d704a506a9b9af318333c501e2d48
diegoski007 <diegopr178@gmail.com>
2019-10-30 11:44:44 -0300

    details

--- app/models/doc.rb
+++ app/models/doc.rb
@@ -1,4 +1,7 @@
 class Doc < ApplicationRecord
   belongs_to :user
   has_one_attached :image, dependent: :destroy
+  validates :title, presence: true
+  
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+
   root to: 'docs#index'
+
   resources :medics
   resources :medictasks
   resources :docs
   resources :members
+  
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     omniauth_callbacks: "users/omniauth_callbacks"
   }
+  
 end
