bedbaf289793fd40bfdcd9ba66d0020b5216d989
diegoski007 <diegopr178@gmail.com>
2019-10-29 23:46:05 -0300

    adjust in fb

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,14 +2,13 @@
 
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   # You should configure your model like this:
-  devise :omniauthable, omniauth_providers: [:facebook]
+  # devise :omniauthable, omniauth_providers: [:facebook]
 
   # You should also create an action method in this controller like this:
   def facebook
     @user = User.from_omniauth(request.env["omniauth.auth"])
     if @user.persisted?
       sign_in_and_redirect @user
-      redirect_to root_path 
       set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
     else
       redirect_to root_path, notice: 'Error al iniciar sesión con Facebook.'
--- db/schema.rb
+++ db/schema.rb
@@ -87,6 +87,7 @@ ActiveRecord::Schema.define(version: 2019_10_30_003817) do
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.datetime "last_sign_in_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
