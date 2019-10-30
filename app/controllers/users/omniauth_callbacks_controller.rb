7d43574990614511fb49423e32d0e8dd4263eea1
diegoski007 <diegopr178@gmail.com>
2019-10-29 22:31:20 -0300

    redirect in fb login

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -8,9 +8,8 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
     @user = User.from_omniauth(request.env["omniauth.auth"])
     if @user.persisted?
-      sign_in_and_redirect @user
-      set_flash_message(:notice, :success, :kind => "Facebook") if
-      is_navigational_format?
+      sign_in_and_redirect root_path
+      set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
     else
       redirect_to root_path, notice: 'Error al iniciar sesión con Facebook.'
     end
